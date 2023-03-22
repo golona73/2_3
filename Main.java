5e580603635d388304301d94de51f1e6f5d7b48c
BOOMER <127319193+golona73@users.noreply.github.com>
2023-03-22 17:08:21 +0000

    Aplikacja działa z funkcja pow

--- Main.java
+++ Main.java
@@ -1,19 +1,20 @@
 import java.util.Random;
+import java.lang.Math;
 
 public class Main {
   public static void main(String[] args) {
     Random random = new Random();
-    int suma = 0;
-    int kwadrat =0;
+    double suma = 0;
+   double kwadrat =0;
 
     for (int i = 0; i < 5; i++) {
-      int number = random.nextInt(10);
+      double number = random.nextInt(10);
       System.out.println("Wylosowana liczba: " + number);
-     kwadrat = number*number;
+     kwadrat = (Math.pow(number, 2));
       suma = suma+kwadrat;
     }
 
-    System.out.println("Suma wylosowanych liczb do kwadratu: " + suma);
+    System.out.println("Suma wylosowanych liczb podniesionych do kwadratu: " + suma);
   }
 }
  
